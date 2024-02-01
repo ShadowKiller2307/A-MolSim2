0a7404abb68c11657120c65cc2ee2e06a50c8cfa
Felix Huber <ge24cob@mytum.de>
2024-02-01 12:42:04 +0100

    added harmonic force class

---
+++ src/physics/pairwiseforces/HarmonicPotential.cpp
@@ -0,0 +1,24 @@
+#include "HarmonicPotential.h"
+
+#include "utils/ArrayUtils.h"
+
+HarmonicPotential::HarmonicPotential(double r0, double k) : r0(r0), r0sqrt(r0 * sqrt2), k(k) {}
+
+std::array<double, 3UL> HarmonicPotential::calculateForce(Particle &p, Particle &q) const { return {0.0, 0.0, 0.0}; };
+
+std::array<double, 3UL> HarmonicPotential::calculateForce(Particle &p, Particle &q, bool is_diagonal) const
+{
+	const double eqilDist = is_diagonal ? r0sqrt : r0;
+	const double distance = ArrayUtils::L2Norm(p.getX() - q.getX());
+
+	return k * (distance - r0) * (1 / distance) * (p.getX() - q.getX());
+}
+
+HarmonicPotential::operator std::string() const { return "Harmonic"; }
+
+void HarmonicPotential::setR0andK(double r0, double k)
+{
+	this->r0 = r0;
+	this->r0sqrt = r0 * sqrt2;
+	this->k = k;
+};
---
+++ src/physics/pairwiseforces/HarmonicPotential.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "physics/pairwiseforces/PairwiseForceSource.h"
+
+/**
+ *  @brief Class to calculate the harmonic force between particles. Implements the interface PairwiseForceSource.
+ *
+ * Implementation of the force calculation to simulate harmonic forces between particles.
+ */
+class HarmonicPotential : public PairwiseForceSource
+{
+private:
+	constexpr static const double sqrt2 = 1.4142135623730951;
+	double r0;	   // equilibrium distance for orthaogonal particles
+	double r0sqrt; // equilibrium distance for diagonal particles
+	double k;	   // stiffness
+
+public:
+	/**
+	 * @brief Constructor for the HarmonicPotential class
+	 * @param r0 The equilibrium distance for orthaogonal particles
+	 * @param k The stiffness
+	 */
+	explicit HarmonicPotential(double r0, double k);
+
+	std::array<double, 3UL> calculateForce(Particle &p, Particle &q) const override;
+
+	/**
+	 * @brief Calculates the harmonic forces between two orthogonal or diagonal particles
+	 *
+	 * @param p Particle
+	 * @param q Particle
+	 * @param is_diagonal true if p and q are diagonal, false if they are orthogonal
+	 * @return harmonic force exerted by q on p
+	 *
+	 * Calculates the harmonic force which q exerts on p
+	 */
+	std::array<double, 3UL> calculateForce(Particle &p, Particle &q, bool is_diagonal) const;
+
+	/**
+	 * @brief Returns "Harmonic" as name of the force
+	 */
+	explicit
+	operator std::string() const override;
+
+	/**
+	 * @brief Sets the equilibrium distance and stiffness
+	 *
+	 * @param r0 The equilibrium distance for orthaogonal particles
+	 * @param k The stiffness
+	 */
+	void setR0andK(double r0, double k);
+};
